d7b34361302f02367917df2073a8bdedaea236ba
Nicolas Escobar <50273150+nicoesve@users.noreply.github.com>
2025-02-24 18:45:39 +0000

    fixing vignette

--- vignettes/riemmtan-package.R
+++
@@ -1,9 +0,0 @@
-#' @keywords internal
-"_PACKAGE"
-
-## usethis namespace: start
-#' @importFrom Matrix Matrix
-#' @importFrom methods as new
-#' @importFrom R6 R6Class
-## usethis namespace: end
-NULL
